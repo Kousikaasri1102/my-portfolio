a9585f479c8b703af918a7d01869977ddc5cccd7
Kousikaasri M <97514179+Kousikaasri1102@users.noreply.github.com>
2022-07-13 22:32:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
-      
+       // sticky navbar on scroll script
+       if(this.scrollY < 500){
+        $('.navbar').addClass("sticky");
+    }else{
+        $('.navbar').removeClass("sticky");
+    }
         // scroll-up button show/hide script
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
